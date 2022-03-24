79e5dd5ee49c58115d0fc44667440d31f38550f3
Cornal <nguyendangduy2210@gmail.com>
2022-03-24 15:41:46 +0700

    gck set up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,11 @@ import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.get('/api/users/currentUser', (req, res) => {
+    return res.send('hi there!');
+})
+
 app.listen(3000, () => {
     console.log(`Listening on port 3000!`);
 })
