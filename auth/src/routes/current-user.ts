5fe86dc4b5b9eac848a9913ef75e5a3ce58286e0
Cornal <nguyendangduy2210@gmail.com>
2022-03-28 01:04:15 +0700

    get current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,9 @@
-import jwt from 'jsonwebtoken';
 import express from 'express';
+import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
-
-router.get('/api/users/current-user', (req, res) => {
-    if(!req.session?.jwt)
-        return res.send({ currentUser: null });
-    try {
-        const payload = jwt.verify(
-            req.session.jwt,
-            process.env.JWT_KEY!
-        )
-        return res.send({ currentUser: payload });
-    } catch (error) {
-        return res.send({ currentUser: null });
-    }
+router.get('/api/users/current-user', currentUser, (req, res) => {
+    return res.send({ currentUser: req.currentUser || null });
 })
 
 export { router as currentUserRouter };
