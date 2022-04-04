76b2a71531d0b81182e0e81b82cf54a615bd0221
Cornal <nguyendangduy2210@gmail.com>
2022-04-04 10:39:13 +0700

    error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/sign-in";
 import { signOutRouter } from "./routes/sign-out";
 import { signUpRouter } from "./routes/sign-up";
+import { errorHandler } from './middlewares/error-handler'
 
 const app = express();
 app.use(json());
@@ -11,6 +12,7 @@ app.use(currentUserRouter)
 app.use(signInRouter)
 app.use(signOutRouter)
 app.use(signUpRouter)
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!!!!");
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator'
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
 const router = express.Router();
 
 router.post('/api/users/sign-up', 
@@ -14,12 +16,14 @@ router.post('/api/users/sign-up',
     ], 
     (req: Request, res: Response) => {
         const errors = validationResult(req);
+
         if (!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
+            throw new RequestValidationError(errors.array());
         }
-        const { email, password } = req.body;
-        console.log(`Creating a user ...`);
-        res.send({}); 
+        console.log("Creating a user...");
+        throw new DatabaseConnectionError();
+
+        res.send({});
     })
 
 export { router as signUpRouter }
