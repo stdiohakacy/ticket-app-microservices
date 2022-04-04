a21273eccb48647546ae0e01d40045baa4a7a8dd
Cornal <nguyendangduy2210@gmail.com>
2022-04-04 18:26:35 +0700

    user creation

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator'
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
 const router = express.Router();
 
 router.post('/api/users/sign-up', 
@@ -14,16 +14,23 @@ router.post('/api/users/sign-up',
             .isLength({ min: 4, max: 20 })
             .withMessage("Password must be between 4 and 20 characters")
     ], 
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array());
         }
-        console.log("Creating a user...");
-        throw new DatabaseConnectionError();
+        const { email, password } = req.body;
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            console.log("Email in use!");
+            return res.send({});
+        }
 
-        res.send({});
+        const user = User.build({ email, password });
+        await user.save();
+
+        return res.status(201).send(user);
     })
 
-export { router as signUpRouter }
+export { router as signUpRouter };
