9b699c27f5e09fc7478aa9a0322839fff6a75a6e
Cornal <nguyendangduy2210@gmail.com>
2022-04-05 17:12:38 +0700

    sign in integrate

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 const router = express.Router();
+import { Request, Response } from 'express';
 
-router.get('/api/users/sign-in', () => {
+router.post('/api/users/sign-in', 
+    [
+        body("email")
+            .isEmail()
+            .withMessage("Email must be valid!"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("You should provide password!")
+    ], 
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
 
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
 })
 
-export { router as signInRouter }
+export { router as signInRouter };
