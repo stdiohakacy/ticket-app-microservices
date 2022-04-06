7a3a42a3925f7aa50d6808e173d3d092e895e40a
Cornal <nguyendangduy2210@gmail.com>
2022-04-06 13:21:51 +0700

    split app and index file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,24 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
-import 'express-async-errors';
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/sign-in";
-import { signOutRouter } from "./routes/sign-out";
-import { signUpRouter } from "./routes/sign-up";
-import { errorHandler } from './middlewares/error-handler'
 import mongoose from 'mongoose';
-import cookieSession from "cookie-session";
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({ signed: false, secure: true }));
-
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if(!process.env.JWT_KEY) {
--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -1,5 +1,5 @@
 import {Request, Response, NextFunction} from 'express';
-import { NotAuthorizedError } from '../errors/Not-authorized-error';
+import { NotAuthorizedError } from '../errors/not-authorized-error';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
     if (!req.currentUser)
