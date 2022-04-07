7f4728120a8b940b93391e05e25a169e160c4feb
Cornal <nguyendangduy2210@gmail.com>
2022-04-08 01:11:05 +0700

    specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
     return <h1>Landing page</h1>
 }
+
+LandingPage.getInitialProps = async () => {
+    let currentUser = null;
+    if (typeof window === "undefined") {
+        // we are on the server!
+        // requests should be made to http://ingress-nginx.ingress-nginx...
+        const { data } = await axios.get("http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/current-user", { 
+            headers: { 
+                Host: 'ticketing.dev',
+                'Content-Type': 'application/json'
+            } 
+        });
+        currentUser = data;
+    } else {
+        // we are on the browser
+        // requests can be made with a base url of ''
+        const { data } = await axios.get("/api/users/current-user");
+        currentUser = data;
+    }
+
+    return currentUser;
+}
+
+export default LandingPage;
