018ea31b0cb33f956c49de59002aaf63c6c67afc
Cornal <nguyendangduy2210@gmail.com>
2022-04-08 15:13:41 +0700

    conditionally showing links

--- client/components/header.js
+++ client/components/header.js
@@ -1,6 +1,20 @@
 import Link from 'next/link'
 
 export default ({ currentUser }) => {
+    const links = [
+        !currentUser && { label: "Sign up", href: "/auth/sign-up" },
+        !currentUser && { label: "Sign in", href: "/auth/sign-in" },
+        currentUser && { label: "Sign out", href: "/auth/sign-out" }
+    ]
+        .filter(linkConfig => linkConfig)
+        .map(({ label, href }) => (
+            <li key={href}>
+                <Link href={href}>
+                    <a className="nav-link">{label}</a>
+                </Link>
+            </li>
+        ))
+
     return (
         <nav className="navbar navbar-light bg-light">
             <Link href="/">
@@ -9,7 +23,7 @@ export default ({ currentUser }) => {
 
             <div className="d-flex justify-content-end">
                 <ul className="nav d-flex align-items-center">
-                    { currentUser ? "Sign out" : "Sign in/up" }
+                    {links}
                 </ul>
             </div>
         </nav>
