d2694d541511313e051434b8349e230740d8094a
Cornal <nguyendangduy2210@gmail.com>
2022-04-10 15:58:05 +0700

    running ticket service

---
+++ tickets/src/app.ts
@@ -0,0 +1,14 @@
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import express from "express";
+import 'express-async-errors';
+import { errorHandler } from '@ticketing-dev-org/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" }));
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,45 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app'
+
+let mongod: any
+
+// declare global {
+//     namespace NodeJS {
+//         interface Global {
+//             signIn(): Promise<string[]>
+//         }
+//     }
+// }
+
+declare global {
+    var signIn: () => Promise<string[]>;
+}
+
+beforeAll(async () => {
+    process.env.JWT_KEY = "secret_key"
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+    mongod = await MongoMemoryServer.create();
+    const mongoUri = mongod.getUri();
+    await mongoose.connect(mongoUri);
+})
+
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+    for (const collection of collections) {
+        await collection.deleteMany({})
+    }
+})
+
+global.signIn = async () => {
+    const email = "admin@gmail.com";
+    const password = "123123"
+
+    const authResponse = await request(app)
+        .post("/api/users/sign-up")
+        .send({ email, password })
+        .expect(201)
+
+    return authResponse.get("Set-Cookie");
+}
