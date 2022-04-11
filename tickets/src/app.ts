e8b87f75fe269a28129ddc36442ce46c1bc0b050
Cornal <nguyendangduy2210@gmail.com>
2022-04-11 16:46:41 +0700

    better error logs

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,12 +4,14 @@ import express from "express";
 import 'express-async-errors';
 import { errorHandler, NotFoundError, currentUser } from '@ticketing-dev-org/common';
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" }));
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 app.use(errorHandler);
 app.use(currentUser)
 export { app };
