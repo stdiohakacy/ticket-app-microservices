839f47335925d2f970484682fbdc5a1322c427d0
Cornal <nguyendangduy2210@gmail.com>
2022-04-11 12:23:12 +0700

    validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
-import { currentUser, requireAuth } from '@ticketing-dev-org/common';
+import { currentUser, requireAuth, validateRequest } from '@ticketing-dev-org/common';
 const router = express.Router();
+import { body } from 'express-validator'
 
-router.post("/api/tickets", currentUser, requireAuth, (req: Request, res: Response) => {
-    res.sendStatus(200);
-})
+router.post(
+    "/api/tickets", 
+    currentUser, 
+    requireAuth, 
+    [
+        body("title").not().isEmpty().withMessage("Title is required"),
+        body("price").isFloat({ gt: 0 }).withMessage("Price must be greater than 0"),
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
+        res.sendStatus(200);
+    }
+)
 
 export { router as createTicketRouter }
