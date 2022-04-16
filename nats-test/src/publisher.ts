0c43e92eb89614e488d6227e53dd5cf6ac773840
Cornal <nguyendangduy2210@gmail.com>
2022-04-16 20:46:30 +0700

    successful listener

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -3,7 +3,7 @@ import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
-const stan = nats.connect('ticketing', 'abc', { url: 'http://localhost:4222', });
+const stan = nats.connect('ticketing', 'abcdef', { url: 'http://localhost:4222', });
 
 stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
