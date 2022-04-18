b56a18e391172db51e8278d6ee55892baae46a38
Cornal <nguyendangduy2210@gmail.com>
2022-04-18 19:21:32 +0700

    NATS env var

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,14 +3,23 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
-  if(!process.env.JWT_KEY) {
+  if (!process.env.JWT_KEY) {
     throw new Error('JWT KEY must be defined!');
   }
-  if(!process.env.MONGO_URI) {
+  if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined!');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS CLIENT ID must be defined!");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS URL must be defined!");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS CLUSTER ID must be defined!");
+  }
   try {
-    await natsWrapper.connect("ticketing", "abc", "http://nats-srv:4222")
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
       process.exit();
