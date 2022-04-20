ec72aa04e17c9f586af91ddbe7c78fe05f9f5995
Cornal <nguyendangduy2210@gmail.com>
2022-04-21 01:10:29 +0700

    cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,8 +1,21 @@
+import { currentUser, NotAuthorizedError, NotFoundError, OrderStatus, requireAuth } from '@ticketing-dev-org/common';
 import express, { Request, Response} from 'express';
+import { Order } from '../models/order';
 const router = express.Router();
 
-router.delete("/api/orders/:id", (req: Request, res: Response) => {
-    return res.send({})
+router.delete("/api/orders/:id", currentUser, requireAuth, async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError()
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
 })
 
 export { router as deleteOrderRouter }
