594d52834961a753646928ddc13d4b48fc06d56f
Cornal <nguyendangduy2210@gmail.com>
2022-04-28 21:51:47 +0700

    touch of k8s setup

---
+++ expiration/src/nats-wrapper.ts
@@ -0,0 +1,28 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+    private _client?: Stan;
+    
+    get client(): Stan {
+        if(!this._client)
+            throw new Error('Cannot access NATS Client before connecting')
+        return this._client
+    }
+
+    connect(clusterId: string, clientId: string, url: string) {
+        this._client = nats.connect(clusterId, clientId, { url });
+        
+
+        return new Promise<void>((resolve, reject) => {
+            this.client.on("connect", () => {
+                console.log(`Connected to NATs`);
+                resolve();
+            })
+            this.client.on("error", (err) => {
+                reject(err);
+            })
+        })
+    }
+}
+
+export const natsWrapper = new NatsWrapper();
