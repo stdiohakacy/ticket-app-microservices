c0df20faf825dca4da6fa83d2cdfd624f597243e
Cornal <nguyendangduy2210@gmail.com>
2022-04-29 12:55:25 +0700

    initial set up payment service

---
+++ payment/src/app.ts
@@ -0,0 +1,21 @@
+import { currentUser, errorHandler } from '@ticketing-dev-org/common';
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import express from "express";
+import 'express-async-errors';
+// import { indexTicketRouter } from "./routes";
+// import { createTicketRouter } from './routes/new';
+// import { showTicketRouter } from './routes/show';
+// import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" }));
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(indexTicketRouter);
+// app.use(updateTicketRouter);
+app.use(errorHandler);
+app.use(currentUser)
+export { app };
