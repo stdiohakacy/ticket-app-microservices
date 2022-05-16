80c4b98cf3d27595e41fd47b3b55e8e6213b628b
Cornal <nguyendangduy2210@gmail.com>
2022-05-17 01:11:54 +0700

    displaying the expiration

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,11 +1,11 @@
 import useRequest from '../../hooks/use-request';
-
+import Router from 'next/router';
 const TicketShow = ({ ticket }) => {
     const { doRequest, errors } = useRequest({
         url: "/api/orders",
         method: "post",
         body: { ticketId: ticket.id },
-        onSuccess: (order) => console.log(order)
+        onSuccess: (order) => Router.push("/orders/[orderId]", `/orders/${order.id}`)
     })
 
     return (
