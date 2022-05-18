657d8fc695faada18147956a1f054f60454d77f5
Cornal <nguyendangduy2210@gmail.com>
2022-05-18 19:01:33 +0700

    test start script

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log("Starting ...");
+  console.log("Starting application ...");
   if(!process.env.JWT_KEY) {
     throw new Error('JWT KEY must be defined!');
   }
