8ac87aff5cea6539af4ca31ceb2e131bfd3c790e
Cornal <nguyendangduy2210@gmail.com>
2022-05-19 01:19:02 +0700

    test client depl

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default ({ req }) => {
-    if (typeof window === 'undefined') {
+    if (typeof window === "undefined") {
         return axios.create({ 
             baseURL: "http://ingress-nginx.ingress-nginx.svc.cluster.local", 
             headers: req.headers 
